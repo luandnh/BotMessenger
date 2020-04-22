3defc5b1271cc4d09f9adbe32c2d7b30ac91465f
luandnh98 <luandnh98@gmail.com>
2020-04-22 14:00:48 +0700

    Test Html

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ const verifyWebhook = require('./verify-webhook');
 const messageWebhook = require('./message-webhook');
 
 const app = express();
+app.set('views', __dirname + '\\views');
+app.engine('html', require('ejs').renderFile);
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => console.log(`Express server is listening on port ${PORT}`));
 
-app.get('/',verifyWebhook);
-
-app.post('/',messageWebhook);
+app.get('/', verifyWebhook);
+app.get('/license', function (req, res) {
+    res.render('index.html');
+});
+app.post('/', messageWebhook);
